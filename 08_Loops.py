37dccf0d4eb4969edd0020946659e232c777e56f
abhimithun4u <abhimithun4u@gmail.com>
2024-02-25 22:58:11 +0530

    loops

---
+++ 08_Loops.py
@@ -0,0 +1,8 @@
+# Author: Abhishek Mukherjee
+# Topic: Loops in Python while and for loops
+
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1
+print("While loop completed")
---
+++ 08_Practice_01_print items in list using while loop.py
@@ -0,0 +1,9 @@
+# Author: Abhishek Mukherjee
+# Topic: Print all contents within a list using a while loop
+fruits = ["Banana", "Apple","Mango","Grapes","Guava","Kiwi","Orange"]
+i = 0
+length = len(fruits)
+print(length)
+while i < length:
+    print(fruits[i])
+    i = i +1
