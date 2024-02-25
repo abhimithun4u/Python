bfde16e796e4ab725823c658b7e4ed1eb20f59ce
abhimithun4u <abhimithun4u@gmail.com>
2024-02-25 21:59:47 +0530

    25_02_24_LeapYear

--- 07_Conditional_Expressions_LeapYear.py
+++ 07_Conditional_Expressions_LeapYear.py
@@ -2,9 +2,7 @@
 # Conditional Expressions : if, elif, else to calculate leap year
 
 num = int(input("Enter the Year: "))
-if num % 4 == 0 and num % 400 == 0:
-    print("The year ", num, " is a leap year!")
-elif num % 100 == 0 and num % 400 == 0:
+if num % 4 == 0 and (num % 100 !=0 or num % 400 ==0):
     print("The year ", num, " is a leap year!")
 else:
     print("The year ", num, " is a not a leap year!")
